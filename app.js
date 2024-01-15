a913ea6abf7567e9607e305c0eedc1d0a76f4a7f
Sakdipat Ontoum <sakdipat3536@gmail.com>
2024-01-15 21:37:16 +0700

    ADD 404 page

--- app.js
+++ app.js
@@ -10,8 +10,11 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(adminRoutes);
-
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
 
 
 app.use('/', (req, res, next) => {

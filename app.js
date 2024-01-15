9eff33e27404dc796814e03bf4fce1c460a8ec73
Sakdipat Ontoum <sakdipat3536@gmail.com>
2024-01-15 21:34:03 +0700

    [ADD] express router

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
+app.use(adminRoutes);
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
+app.use(shopRoutes);
+
 
 app.use('/', (req, res, next) => {
   res.send('<h1>Hello from Express!</h1>');

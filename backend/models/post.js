a6d3c9e62ce6650b539ccbf4b836f510faddc9e7
Sakdipat Ontoum <sakdipat3536@gmail.com>
2024-01-20 12:05:51 +0700

    [ADD]

--- backend/models/post.js
+++ backend/models/post.js
@@ -5,7 +5,7 @@ const pool = require('../controllers/db'); // Adjust the path accordingly
 const createPostsTable = async () => {
   const createTableQuery = `
     CREATE TABLE IF NOT EXISTS posts (
-      "id" SERIAL PRIMARY KEY,
+      "postId" SERIAL PRIMARY KEY,
       "title" VARCHAR(255) NOT NULL,
       "imageUrl" VARCHAR(255) NOT NULL,
       "content" TEXT NOT NULL,
